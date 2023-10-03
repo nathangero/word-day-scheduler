f1b11418200aab495a5fbeb95a8d888a7404c359
nathangero <nathangero@yahoo.com>
2023-10-03 15:55:57 -0700

    implement showing and color coding blocks based on time

--- script.js
+++ script.js
@@ -22,12 +22,15 @@ $(function () {
   for (let i = 0; i <= TIME_BLOCKS_COUNT; i++) {
     let hour = (i + 9);
     divId += hour; // Calculate the id
+    let hourAmPm = getAmPm(hour);
+    let timeStatus = determinePastPresentFuture(hour); // Past, Present, or Future
 
-    var newBlock = $(`<div id=${divId} class="row time-block past"></div>`);
-    var blockTime = $(`<div class="col-2 col-md-1 hour text-center py-3">${getAmPm(hour)}</div>`);
-    var blockTextArea = $(`<textarea class="col-8 col-md-10 description" rows="3"> </textarea>`);
-    var blockSaveButton = $(`<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button`);
-    var saveButtonIcon = $(`<i class="fas fa-save" aria-hidden="true"></i>`);
+
+    let newBlock = $(`<div id=${divId} class="row time-block ${timeStatus}"></div>`);
+    let blockTime = $(`<div class="col-2 col-md-1 hour text-center py-3">${hourAmPm}</div>`);
+    let blockTextArea = $(`<textarea class="col-8 col-md-10 description" rows="3"> </textarea>`);
+    let blockSaveButton = $(`<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button`);
+    let saveButtonIcon = $(`<i class="fas fa-save" aria-hidden="true"></i>`);
 
     blockSaveButton.append(saveButtonIcon); // Add the icon into the button
 
@@ -73,5 +76,23 @@ function getAmPm(hour) {
     return hour + " AM";
   } else {
     return (hour - 12) + " PM";
+  }
+}
+
+/**
+ * Compares if the hour is equal to the current time, earlier in the day or later in the day.
+ * The return will determine what css class to give, which will determine what color the time block will be
+ * @param {hour} hour The hour to compare to
+ * @returns Either the string "past", "present", or "future".
+ */
+function determinePastPresentFuture(hour) {
+  let currentHour = today.format("HH");
+
+  if (hour == currentHour) {
+    return "present";
+  } else if (hour < currentHour) {
+    return "past";
+  } else if (hour > currentHour) {
+    return "future";
   }
 }
