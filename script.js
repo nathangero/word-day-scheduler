615515c2244c3703f0c20b460ae002ad6f3c33cc
nathangero <nathangero@yahoo.com>
2023-10-03 16:05:33 -0700

    remove comment

--- script.js
+++ script.js
@@ -44,16 +44,6 @@ $(function () {
   }
 
 
-  /**
-   * 
-   * <div id="hour-9" class="row time-block past">
-        <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
-        <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-        <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-          <i class="fas fa-save" aria-hidden="true"></i>
-        </button>
-      </div>
-   */
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
