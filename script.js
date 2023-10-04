57f93e5e2993729d7b785e070cbd4827577705b0
nathangero <nathangero@yahoo.com>
2023-10-03 17:55:02 -0700

    refactor createTimeBlocks() to be its own function

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-const TIME_BLOCKS_COUNT = 8; // 8 hours/blocks
+const TIME_BLOCKS_COUNT = 10; // 8 hours/blocks
 const STORAGE_STRING_EVENTS = "userEvents";
 
 // Key = div id, value = textarea string
 var events = {}
 
 var today = dayjs();
-var calendar = $("#calendar");
+var currentHour = today.format("HH");
+
+
+function createTimeBlocks() {
+  var calendar = $('<div id="calendar" class="container-lg px-5">');
+
+  let divId = "hour-"; // Used to mark time block
+  for (let i = 0; i <= TIME_BLOCKS_COUNT; i++) {
+    let hour = (i + 9);
+    divId += hour; // Calculate the id
+    let hourAmPm = getAmPm(hour);
+    let timeStatus = determinePastPresentFuture(hour); // Past, Present, or Future
+    let event = events[divId] ? events[divId] : ""; // Check if event exists, if not then give an empty string
+
+    let newBlock = $(`<div id=${divId} class="row time-block"></div>`);
+    newBlock.addClass(timeStatus);
+
+    let blockTime = $(`<div class="col-2 col-md-1 hour text-center py-3"></div>`);
+    blockTime.text(hourAmPm);
+
+    let blockTextArea = $(`<textarea class="col-8 col-md-10 description" rows="3"></textarea>`);
+    blockTextArea.text(event)
+
+    let blockSaveButton = $(`<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button`);
+    blockSaveButton.click(saveEvent); // Add click listener
+
+    let saveButtonIcon = $(`<i class="fas fa-save" aria-hidden="true"></i>`);
+
+    blockSaveButton.append(saveButtonIcon); // Add the icon into the button
+
+    // Add all the elemnts into the block
+    newBlock.append(blockTime);
+    newBlock.append(blockTextArea);
+    newBlock.append(blockSaveButton);
+
+    // Add the new block to the calendar
+    calendar.append(newBlock);
+    divId = "hour-"; // reset the id to default before the next iteration runs
+  }
+
+  return calendar;
+}
 
 
 // Determine if hour should have AM or PM after it.
@@ -27,8 +68,8 @@ function getAmPm(hour) {
  * @returns Either the string "past", "present", or "future".
  */
 function determinePastPresentFuture(hour) {
-  let currentHour = today.format("HH");
-
+  console.log("@determinePastPresentFuture");
+  console.log(currentHour)
   if (hour == currentHour) {
     return "present";
   } else if (hour < currentHour) {
@@ -64,47 +105,25 @@ function loadEvents() {
 
 
 $(function () {
-  let divId = "hour-"; // Used to mark time block
-  for (let i = 0; i <= TIME_BLOCKS_COUNT; i++) {
-    let hour = (i + 9);
-    divId += hour; // Calculate the id
-    let hourAmPm = getAmPm(hour);
-    let timeStatus = determinePastPresentFuture(hour); // Past, Present, or Future
-    let event = events[divId] ? events[divId] : ""; // Check if event exists, if not then give an empty string
+  let calendar = createTimeBlocks();
+  $("main").append(calendar)
+  
 
-    let newBlock = $(`<div id=${divId} class="row time-block"></div>`);
-    newBlock.addClass(timeStatus);
+  $("#currentDay").text(today.format("dddd MMMM DD, YYYY HH:MM:ss"));
 
-    let blockTime = $(`<div class="col-2 col-md-1 hour text-center py-3"></div>`);
-    blockTime.text(hourAmPm);
-
-    let blockTextArea = $(`<textarea class="col-8 col-md-10 description" rows="3"></textarea>`);
-    blockTextArea.text(event)
-
-    let blockSaveButton = $(`<button class="btn saveBtn col-2 col-md-1" aria-label="save"></button`);
-    blockSaveButton.click(saveEvent); // Add click listener
-
-    let saveButtonIcon = $(`<i class="fas fa-save" aria-hidden="true"></i>`);
-
-    blockSaveButton.append(saveButtonIcon); // Add the icon into the button
-
-    // Add all the elemnts into the block
-    newBlock.append(blockTime);
-    newBlock.append(blockTextArea);
-    newBlock.append(blockSaveButton);
-
-    // Add the new block to the calendar
-    calendar.append(newBlock);
-    divId = "hour-"; // reset the id to default before the next iteration runs
-  }
-
-  $("#currentDay").text(today.format("dddd MMMM DD, YYYY"));
-
-
+  
   // Do bonus? Change seconds? Maybe change the colors of the blocks every new hour?
   // setInterval(() => {
-  //   today = dayjs();
-  //   $("#currentDay").text(today.format("dddd MMMM DD, YYYY HH:MM:ss"));
+  //   let timerTime = dayjs();
+  //   let timerHour = timerTime.format("HH");
+
+  //   if (timerHour !== currentHour) {
+  //     currentHour = timerHour;
+  //     $("#calendar").remove(); // Remove old calendar
+  //     let calendar = createTimeBlocks();
+  //     $("main").append(calendar) // Add back new calendar
+  //   }
+  //   $("#currentDay").text(timerTime.format("dddd MMMM DD, YYYY HH:MM:ss"));
   // }, 1000);
 });
 
